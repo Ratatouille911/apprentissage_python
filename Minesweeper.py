a1b4b1826b6741f4d881a0bc01bccbf819e518e7
Ratatouille911 <jacquesjean911@yahoo.com>
2021-04-21 21:05:46 +0000

    added game beginning

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,2 +1,5 @@
 #aim = building a home made minesweeper
-print("Hello") 
+grid_dimension = {10:[9,9], 40:[16,16], 99:[16,30]} #defines the grid dimensions in fonction of the number of mines
+print("Hello")
+mines_number = input("How many mines do you want to find ? (10, 40, 99) : ") 
+print("Here is your {} mines grid, good luck !".format(mines_number))
